a80b84e2f65c81e7ecf8e158387065c46797e544
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-06-13 16:07:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,24 +1,24 @@
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("bobo", 32, "male", "brown", "friendly", true);
+VALUES ("Mr. Chocolate", 32, "male", "brown", "friendly", true);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("bobo2", 26, "female", "brown", "not friendly", true);
+VALUES ("Rowdy", 26, "female", "brown", "not friendly", true);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("bobo3", 22, "male", "black", "friendly", true);
+VALUES ("Tabitha", 22, "male", "black", "friendly", true);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("bobo4", 53, "male", "black", "not friendly", false);
+VALUES ("Sergeant Brown", 53, "male", "black", "not friendly", false);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("bobo5", 2, "male", "kodiak", "angry", true);
+VALUES ("Melissa", 2, "male", "kodiak", "angry", true);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("bobo6", 36, "male", "brown", "sleepy", true);
+VALUES ("Grinch", 36, "male", "brown", "sleepy", true);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("bobo7", 74, "female", "arctic", "friendly", true);
+VALUES ("Wendy", 74, "female", "arctic", "friendly", true);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("bobo8", 64, "male", "brown", "friendly", true);
+VALUES (Null, 64, "male", "brown", "friendly", true);
