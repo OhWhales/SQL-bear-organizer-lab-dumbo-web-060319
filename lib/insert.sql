8a46f7af1fbbbb69f952f05621c0ed3bf2dcbd77
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-06-13 16:16:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -20,4 +20,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Wendy", 19, "Male", "Brown", "Friendly", true);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Mr. Chocolate", 19, "Male", "Brown", "Friendly", true);
+VALUES (NULL, 19, "Male", "Brown", "Friendly", true);
